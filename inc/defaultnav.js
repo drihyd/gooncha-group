99095b932f0d318690a4a0157ad3917fb90e4c0f
drihyd <techsupport@deepredink.com>
2021-11-09 16:30:27 +0530

    changes done

--- inc/defaultnav.js
+++ inc/defaultnav.js
@@ -2,8 +2,8 @@
 const Header = ({ headerNav }) =>
 `
     <header class="main-header" id="mainheader">
-      <div class="container">
-        <nav class="navbar navbar-expand-lg justify-content-between">
+      <div class="container pr-sm-0">
+        <nav class="navbar navbar-expand-lg justify-content-between pl-sm-0 pr-sm-0">
               <a class="navbar-brand" href="./"><img src="./img/logo.png" alt="Gooncha Group Logo" class="img-fluid header-logo"></a>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fa fa-bars" aria-hidden="true"></i>
@@ -11,16 +11,16 @@ const Header = ({ headerNav }) =>
               <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav ml-auto">
                   <li class="nav-item link-about">
-                    <a class="nav-link" href="#">About Us</a>
+                    <a class="nav-link" href="./about-us.html">About Us</a>
                   </li>
                   <li class="nav-item link-business">
-                    <a class="nav-link" href="#">Business</a>
+                    <a class="nav-link" href="./management.html">Business</a>
                   </li>
                   <li class="nav-item link-csr">
                     <a class="nav-link" href="#">CSR</a>
                   </li>
                   <li class="nav-item link-media">
-                    <a class="nav-link" href="#">Media</a>
+                    <a class="nav-link" href="./media.html">Media</a>
                   </li>
                   <li class="nav-item link-contact">
                     <a class="nav-link" href="#">Contact</a>
